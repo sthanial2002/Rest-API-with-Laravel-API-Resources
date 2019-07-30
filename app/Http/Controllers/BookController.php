09b7b933e480e3b19f48e1de0dc3d8218fdc3205
Maurel OUSSOU <micronet@consultant.com>
2019-07-30 14:57:35 +0100

    first commit

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Book;
+use App\Http\Resources\BookResource;
+
+class BookController extends Controller
+{
+
+    public function __construct()
+    {
+      $this->middleware('auth:api')->except(['index', 'show']);
+    }
+
+
+    
+    public function index()
+    {
+      return BookResource::collection(Book::with('ratings')->paginate(25));
+    }
+
+    public function store(Request $request)
+    {
+      $book = Book::create([
+        'user_id' => $request->user()->id,
+        'title' => $request->title,
+        'description' => $request->description,
+      ]);
+
+      return new BookResource($book);
+    }
+
+    public function show(Book $book)
+    {
+      return new BookResource($book);
+    }
+
+    public function update(Request $request, Book $book)
+    {
+      // check if currently authenticated user is the owner of the book
+      if ($request->user()->id !== $book->user_id) {
+        return response()->json(['error' => 'You can only edit your own books.'], 403);
+      }
+
+      $book->update($request->only(['title', 'description']));
+
+      return new BookResource($book);
+    }
+
+    public function destroy(Book $book)
+    {
+      $book->delete();
+
+      return response()->json(null, 204);
+    }
+}
---
+++ app/Http/Controllers/RatingController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Book;
+use App\Models\Rating;
+use App\Http\Resources\RatingResource;
+
+class RatingController extends Controller
+{
+
+    public function __construct()
+    {
+      $this->middleware('auth:api');
+    }
+
+    
+    public function store(Request $request, Book $book)
+    {
+      $rating = Rating::firstOrCreate(
+        [
+          'user_id' => $request->user()->id,
+          'book_id' => $book->id,
+        ],
+        ['rating' => $request->rating]
+      );
+
+      return new RatingResource($rating);
+    }
+}
