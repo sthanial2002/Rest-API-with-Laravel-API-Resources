8a77769ecc0119c295cba32f2f49355adf1a199a
Maurel OUSSOU <micronet@consultant.com>
2019-08-06 17:26:41 +0100

    Update config/app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://rest-api-laravel5.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
